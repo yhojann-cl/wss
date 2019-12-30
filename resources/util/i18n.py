ad60a7a337d80c724c0499bf5b1519cd0a2575dc
codeleaks <admin@undernode.com>
2019-12-30 18:46:55 -0500

    i18n module

---
+++ resources/util/i18n.py
@@ -0,0 +1,15 @@
+import json
+from locale import getdefaultlocale
+
+
+def get_locale():
+    __lang = getdefaultlocale()[0].split('_')[0].lower()
+    return str(__lang)
+
+
+def lang_from_path(path):
+    with open(path, 'r') as f:
+        try:
+            return json.load(f)
+        except Exception as e:
+            return None
