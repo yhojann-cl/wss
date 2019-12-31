c8c515fda506da9dc6d3725c3c267883d53e91b7
codeleaks <admin@undernode.com>
2019-12-31 15:56:31 -0500

    define class

--- resources/util/helpers.py
+++ resources/util/helpers.py
@@ -3,44 +3,45 @@ from time import strftime, localtime, time
 from pyfiglet import Figlet
 
 
-def lang_exists(file):
-    return path.isfile(file) is True
+class Helper(object):
+    """
+    Helper class for common runtime
+    """
+    def __init__(self):
+        pass
 
+    def langexists(self, file):
+        return path.isfile(file) is True
 
-def get_lang_value(buf, p, c):
-    if isinstance(buf, dict):
-        parent = buf.get(p)
-        if isinstance(parent, dict):
-            return parent.get(c)
-        elif isinstance(parent, list):
-            s = ", "
-            s.join(parent)
-            return s
+    def getlang(self, buf, p, c):
+        if isinstance(buf, dict):
+            parent = buf.get(p)
+            if isinstance(parent, dict):
+                return parent.get(c)
+            elif isinstance(parent, list):
+                s = ", "
+                s.join(parent)
+                return s
+            else:
+                return parent
         else:
-            return parent
-    else:
-        return None
+            return None
 
+    def version(self):
+        version = {'major': 2, 'minor': 6}
+        s = '.'
+        return s.join(['%s' % v for (k, v) in version.items()])
 
-def version():
-    version = {'major': 2, 'minor': 6}
-    s = '.'
-    return s.join(['%s' % v for (k, v) in version.items()])
+    def dtos(self, args):
+        s = ', '
+        return s.join(['%s' % v for v in args.keys()])
 
+    def gettime(self):
+        return strftime('%b %e, %Y %T', localtime(time()))
 
-def dtos(args):
-    s = ', '
-    return s.join(['%s' % v for v in args.keys()])
+    def ftext(self, s, font='slant'):
+        f = Figlet(font=font)
+        return f.renderText(s)
 
-
-def gettime():
-    return strftime('%b %e, %Y %T', localtime(time()))
-
-
-def figlet_text(s, font='slant'):
-    f = Figlet(font=font)
-    return f.renderText(s)
-
-
-def formatter(fmt, args):
-    return fmt.format(*args)
+    def formatter(self, fmt, args):
+        return fmt.format(*args)
