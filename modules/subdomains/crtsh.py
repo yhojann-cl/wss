39a12873c529048e97d228a607fcdf4d5d903b68
codeleaks <admin@undernode.com>
2020-01-05 16:54:51 -0500

    module refactor

--- modules/subdomains/crtsh.py
+++ modules/subdomains/crtsh.py
@@ -3,31 +3,15 @@
 
 import json
 from modules.helpers.networking.crawler import WCrawler
+from resources.util.helpers import Helper
 
 
 class MethodCrtSh:
+    def __init__(self):
+        pass
 
-    def __init__(self, context):
-
-        # El contexto principal
-        self.context = context
-
-        # Variable que permite entregar subdominios únicos (no duplicados)
-        self.hostnames = []
-
-
-    def find(self):
-
-        # Mensaje de la cabecera del método
-        self.context.out(
-            message=self.context.strings['method-begin'],
-            parseDict={
-                'current' : self.context.progress['methods']['current'],
-                'total'   : self.context.progress['methods']['total'],
-                'title'   : self.context.strings['methods']['crt-sh']['title']
-            }
-        )
-
+    def find(self, hostname):
+        h = Helper()
         # Uso del crawler
         crawler = WCrawler()
         crawler.defaultTimeout = 60
@@ -35,61 +19,40 @@ class MethodCrtSh:
         # El resultado es de tipo json
         result = None
 
+        req = h.formatter('https://crt.sh/?q=%.{}&output=json', [hostname])
+
         try:
-            result = crawler.httpRequest(
-                url='https://crt.sh/?q=' + crawler.urlencode('%.' + self.context.baseHostname) + '&output=json'
-            )
+            result = crawler.httpRequest(req)
 
             # Libera la memoria (no es necesario un contexto de navegación)
             crawler.clearContext()
 
         except Exception as e:
-            self.context.out(
-                self.context.strings['methods']['crt-sh']['no-connect']
-            )
-            return
+            return []
 
         # ¿La respuesta HTTP es OK?
-        if(result['status-code'] != 200):
-            self.context.out(
-                message=self.context.strings['methods']['crt-sh']['wrong-status-http'],
-                parseDict={
-                    'id': result['status-code']
-                }
-            )
-            return
+        if (result['status-code'] != 200):
+            return []
 
         try:
             # Convierte el resultado en un objeto de tipo json
             result = json.loads(result['response-content'])
 
         except Exception as e:
-            self.context.out(
-                self.context.strings['methods']['crt-sh']['corrupt-response']
-            )
-            return
+            return []
 
-        if(
-            (not isinstance(result, list)) or
-            (len(result) == 0)
-        ):
-            self.context.out(
-                self.context.strings['methods']['crt-sh']['empty']
-            )
-            return
+        if ((not isinstance(result, list)) or (len(result) == 0)):
+            return []
 
+        hostnames = []
         # Procesa cada nombre de dominio encontrado
         for item in result:
 
             # Evita los resultados duplicados utilizando la pila local
-            if(item['name_value'] in self.hostnames):
+            if (item['name_value'] in hostnames):
                 continue
 
             # Agrega el subdominio encontrado a la pila local
-            self.hostnames.append(item['name_value'])
+            hostnames.append(item['name_value'])
 
-            # Agrega el subdominio encontrado a la pila global de resultados
-            self.context.addHostName(
-                hostname=item['name_value'],
-                messageFormat=self.context.strings['methods']['crt-sh']['item-found']
-            )
+        return hostnames
