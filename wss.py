0a9bd04b7352fdeebb97e95347683e389bacd66c
codeleaks <admin@undernode.com>
2020-01-05 21:17:54 -0500

    define method for cli

--- wss.py
+++ wss.py
@@ -4,12 +4,29 @@
 import sys
 import socket
 
+from anytree import Node, RenderTree
+
 from resources.util.constants import constants
 from resources.util.i18n import get_locale, lang_from_path
 from resources.util.helpers import Helper
 from resources.util.logging import Logging, LogLevel
 
 from modules.http.server import HttpServer
+from modules.filters.ports import FilterPorts
+from modules.filters.http import FilterHttpServices
+from modules.filters.rawports import FilterRawPorts
+from modules.subdomains.axfr import MethodAxfr
+from modules.subdomains.dnsqueries import MethodDnsQueries
+from modules.subdomains.virustotal import MethodVirusTotal
+from modules.subdomains.robtex import MethodRobtex
+from modules.subdomains.crtsh import MethodCrtSh
+from modules.subdomains.certificatedetails import MethodCertificateDetails
+from modules.subdomains.google import MethodGoogle
+from modules.subdomains.bing import MethodBing
+from modules.subdomains.dnsdumpster import MethodDnsDumpster
+from modules.subdomains.dictionary import MethodDictionary
+
+hostname = None
 
 
 def show_help(langbuf, param):
@@ -36,17 +53,111 @@ def run_server(langbuf, param):
     http.start()
 
 
+def run_method(langbuf, param):
+    h = Helper()
+    Logging.log(h.getlang(langbuf, 'running', None),
+                            LogLevel.CLI)
+    if param is not None:
+        for p in param:
+            if (p == '0'):
+                axfr = MethodAxfr()
+                res = axfr.find(hostname)
+                #Output
+                AXFR_node = Node('AXFR')
+                NS_node = Node('nameservers', parent=AXFR_node)
+                DOMAIN_node = Node('subdomains', parent=AXFR_node)
+                ns = res.get('ns', [])
+                dn = res.get('domains', [])
+                for n in ns:
+                    node = Node(n, parent=NS_node)
+                for d in dn:
+                    node = Node(d, parent=DOMAIN_node)
+
+                for pre, fill, node in RenderTree(AXFR_node):
+                    Logging.log(h.formatter("{}{}", [pre, node.name]),
+                                LogLevel.CLI)
+
+            elif (p == '1'):
+                dnsq = MethodDnsQueries()
+                res = dnsq.find(hostname)
+                #Output
+                DNSQ_node = Node('DNS Queries')
+                for r in res:
+                    node = Node(h.formatter('{} // {}', [
+                        r.get('subdomain', '<n/a>'),
+                        r.get('recordType', '<n/a>')
+                    ]),
+                                parent=DNSQ_node)
+                for pre, fill, node in RenderTree(DNSQ_node):
+                    Logging.log(h.formatter("{}{}", [pre, node.name]),
+                                LogLevel.CLI)
+            elif (p == '2'):
+                virustotal = MethodVirusTotal()
+                res = virustotal.find(hostname)
+                #Output
+                VIRUSTOTAL_node = Node('VirusTotal')
+                for v in res:
+                    node = Node(v, parent=VIRUSTOTAL_node)
+                for pre, fill, node in RenderTree(VIRUSTOTAL_node):
+                    Logging.log(h.formatter("{}{}", [pre, node.name]),
+                                LogLevel.CLI)
+            elif (p == '3'):
+                robtex = MethodRobtex()
+                res = robtex.find(hostname)
+                #Output
+                ROBTEX_node = Node('RobTex')
+                for r in res:
+                    node = Node(r, parent=ROBTEX_node)
+                for pre, fill, node in RenderTree(ROBTEX_node):
+                    Logging.log(h.formatter("{}{}", [pre, node.name]),
+                                LogLevel.CLI)
+            elif (p == '4'):
+                crtsh = MethodCrtSh()
+                res = crtsh.find(hostname)
+                #Output
+                CRTSH_node = Node('CRTSH')
+                for c in res:
+                    node = Node(c, parent=CRTSH_node)
+                for pre, fill, node in RenderTree(CRTSH_node):
+                    Logging.log(h.formatter("{}{}", [pre, node.name]),
+                                LogLevel.CLI)
+
+
+def run_filter(langbuf, param):
+    print(param)
+
+
+def define_host(langbuf, param):
+    global hostname
+    h = Helper()
+    if h.hostname_validate(param) is None:
+        Logging.log(h.getlang(langbuf, 'errors', 'invalid-hostname'),
+                    LogLevel.DANGER)
+        exit(-1)
+    else:
+        hostname = param
+
+
 class Wss(object):
 
     langbuf = None
     lang = None
     langpath = None
     CMDS = [{
-        "flags": ['s', 'server'],
-        "runtime": run_server
-    }, {
         "flags": ['h', 'help'],
         "runtime": show_help
+    }, {
+        "flags": ['host'],
+        "runtime": define_host
+    }, {
+        "flags": ['a', 'api'],
+        "runtime": run_server
+    }, {
+        "flags": ['m', 'methods'],
+        "runtime": run_method
+    }, {
+        "flags": ['f', 'filters'],
+        "runtime": run_filter
     }]
 
     def __init__(self):
